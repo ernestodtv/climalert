a9cf060152267fb7ef7d321b10b56d3e293db515
ernesto <ernestodtv@gmail.com>
2026-03-09 14:04:27 +0100

    Initial project setup

---
+++ src/main/java/dev/ernesto/climalert/ClimalertApplication.java
@@ -0,0 +1,13 @@
+package dev.ernesto.climalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimalertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimalertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ernesto/climalert/ClimalertApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ernesto.climalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimalertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
